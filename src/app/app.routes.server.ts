f42911a0770979cb25497a917c93c9c4e97bdd3f
Cosm0666 <155865190+Cosm0666@users.noreply.github.com>
2025-10-07 11:19:37 -0300

    render

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,13 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Server
-  }
+  
+  { path: '/home', renderMode: RenderMode.Prerender },
+  { path: '/movies', renderMode: RenderMode.Prerender },
+  { path: '/tv', renderMode: RenderMode.Prerender },
+  { path: '/movie/:id', renderMode: RenderMode.Server },
+  { path: '/tv/:id', renderMode: RenderMode.Server },
+  { path: '/search/:query', renderMode: RenderMode.Server },
+  { path: '**', renderMode: RenderMode.Server },
+  
 ];
