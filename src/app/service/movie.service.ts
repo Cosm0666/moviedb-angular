670a6f6321abca6c69631df02e56809c368c0db9
Cosm0666 <155865190+Cosm0666@users.noreply.github.com>
2025-10-09 11:06:00 -0300

    fix2

--- src/app/service/movie.service.ts
+++ src/app/service/movie.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { Observable, of } from 'rxjs';
 import {
   MovieCreditsResponse,
   MovieDetails,
@@ -19,9 +20,29 @@ export class MovieService {
   private apiUrl = environment.apiUrl;
   private apiKey = environment.apiKey;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: any
+  ) {}
+
+  
+  private isServer(): boolean {
+    return isPlatformServer(this.platformId);
+  }
+
+
+  private handleServerRequest<T>(): Observable<T> {
+    if (this.isServer()) {
+      console.log('Servidor: Evitando requisição HTTP');
+      return of({} as T);
+    }
+    return null as any;
+  }
 
   getPopularMovies(page: number = 1): Observable<MovieResponse> {
+    const serverResponse = this.handleServerRequest<MovieResponse>();
+    if (serverResponse) return serverResponse;
+
     return this.http.get<MovieResponse>(`${this.apiUrl}/movie/popular`, {
       params: {
         api_key: this.apiKey,
@@ -31,7 +52,11 @@ export class MovieService {
       },
     });
   }
+
   searchMovies(query: string): Observable<MovieSearchResponse> {
+    const serverResponse = this.handleServerRequest<MovieSearchResponse>();
+    if (serverResponse) return serverResponse;
+
     return this.http.get<MovieSearchResponse>(`${this.apiUrl}/search/movie`, {
       params: {
         api_key: this.apiKey,
@@ -40,7 +65,11 @@ export class MovieService {
       },
     });
   }
+
   getMovieDetails(id: number): Observable<MovieDetails> {
+    const serverResponse = this.handleServerRequest<MovieDetails>();
+    if (serverResponse) return serverResponse;
+
     return this.http.get<MovieDetails>(`${this.apiUrl}/movie/${id}`, {
       params: {
         api_key: this.apiKey,
@@ -49,7 +78,11 @@ export class MovieService {
       },
     });
   }
+
   getMoviesVideos(id: number): Observable<{ results: any[] }> {
+    const serverResponse = this.handleServerRequest<{ results: any[] }>();
+    if (serverResponse) return serverResponse;
+
     return this.http.get<{ results: any }>(
       `${this.apiUrl}/movie/${id}/videos`,
       {
@@ -60,7 +93,11 @@ export class MovieService {
       }
     );
   }
+
   getCredits(id: number): Observable<MovieCreditsResponse> {
+    const serverResponse = this.handleServerRequest<MovieCreditsResponse>();
+    if (serverResponse) return serverResponse;
+
     return this.http.get<MovieCreditsResponse>(
       `${this.apiUrl}/movie/${id}/credits`,
       {
@@ -72,9 +109,10 @@ export class MovieService {
     );
   }
 
-
-  
   getSeriesCredits(series_id: number): Observable<SerieCreditsResponse> {
+    const serverResponse = this.handleServerRequest<SerieCreditsResponse>();
+    if (serverResponse) return serverResponse;
+
     return this.http.get<SerieCreditsResponse>(
       `${this.apiUrl}/tv/${series_id}/credits`,
       {
@@ -83,9 +121,13 @@ export class MovieService {
           language: 'pt-BR',
         },
       }
-    )
+    );
   }
+
   searchShows(query: string): Observable<SeriesResponse> {
+    const serverResponse = this.handleServerRequest<SeriesResponse>();
+    if (serverResponse) return serverResponse;
+
     return this.http.get<SeriesResponse>(`${this.apiUrl}/search/tv`, {
       params: {
         api_key: this.apiKey,
@@ -94,7 +136,11 @@ export class MovieService {
       },
     });
   }
+
   getSeriesDetails(series_id: number): Observable<Series> {
+    const serverResponse = this.handleServerRequest<Series>();
+    if (serverResponse) return serverResponse;
+
     return this.http.get<Series>(`${this.apiUrl}/tv/${series_id}`, {
       params: {
         api_key: this.apiKey,
@@ -102,7 +148,11 @@ export class MovieService {
       },
     });
   }
+
   getSeriesVideos(series_id: number): Observable<{ results: any[] }> {
+    const serverResponse = this.handleServerRequest<{ results: any[] }>();
+    if (serverResponse) return serverResponse;
+
     return this.http.get<{ results: any }>(
       `${this.apiUrl}/tv/${series_id}/videos`,
       {
@@ -113,7 +163,11 @@ export class MovieService {
       }
     );
   }
+
   getPopularSeries(page: number = 1): Observable<SeriesResponse> {
+    const serverResponse = this.handleServerRequest<SeriesResponse>();
+    if (serverResponse) return serverResponse;
+
     return this.http.get<SeriesResponse>(`${this.apiUrl}/tv/popular`,
       {
         params: {
@@ -123,6 +177,6 @@ export class MovieService {
           include_adult: 'false',
         }
       }
-    )
+    );
   }
 }
